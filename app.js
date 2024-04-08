7b8f91298e8aa83434004b2a9ece219ab5148bde
‘caodi <853254493@qq.com>
2024-04-08 22:54:37 +0800

    get请求解析

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const app=new Koa()
 
 // 3.创建中间件
 app.use(ctx=>{
+    console.log('=====',ctx.request.query);
+    console.log('=====',ctx.query);
+    console.log('=====',ctx.request.querystring);
     ctx.response.body='hellow world'
 })
 
 app.listen(3000,()=>{
     console.log("当前服务成功");
 })
+
