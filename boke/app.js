7593b1718dbea52ec7fd620d4c22b258342db1b1
‘caodi <853254493@qq.com>
2024-04-27 16:46:43 +0800

    跨域处理

--- boke/app.js
+++ boke/app.js
@@ -4,9 +4,11 @@ const { default: koaBody } = require("koa-body");//处理post
 const router=require('./router') //导入路由
 const mongoMiddleWare=require('./middleware/mongodb')//自定义中间件
 const koaStatic=require('koa-static') //导入处理静态资源-托管中间件 
+const koaCors=require('@koa/cors') //处理跨域问题
 // 创建Koa 实例
 const app = new Koa();
-
+// 中间件：cors 跨域处理
+app.use(koaCors())
 // 中间件：静态资源服务
 app.use(koaStatic('./static'))
 // 中间件：请求体参数处理
