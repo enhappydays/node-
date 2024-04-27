8c8e770361e28a99f98a267b2d638a4576625bc0
‘caodi <853254493@qq.com>
2024-04-27 17:22:41 +0800

    参数校验

--- boke/app.js
+++ boke/app.js
@@ -6,8 +6,11 @@ const mongoMiddleWare=require('./middleware/mongodb')//自定义中间件
 const koaStatic=require('koa-static') //导入处理静态资源-托管中间件 
 const koaCors=require('@koa/cors') //处理跨域问题
 const koaError=require('koa-json-error')//统一错误处理和错误输出
+const koaParameter=require('koa-parameter') //导入参数校验的中间件
 // 创建Koa 实例
 const app = new Koa();
+// 中间件：请求参数校验
+koaParameter(app)
 // 中间件：cors 跨域处理
 app.use(koaCors())
 // 中间件：静态资源服务
@@ -28,6 +31,15 @@ app.use(koaBody({
 app.use(koaError({
     // 自定义接口返回的错误格式
     format(err,obj){
+        console.log(obj);
+        // 企业开发，为了安全性，一般对于参数校验失败，无需告诉前端更多错误细节
+        // 即：只需要告诉前端参数不合法即可
+        if (obj.code==='INVALID_PARAM') {
+            return {
+                code:40022,
+                message:'存在不合法参数！！！'
+            }
+        }
         return {
           code:obj.code||50000,
           message:obj.message||err.message
--- boke/controller/home.js
+++ boke/controller/home.js
@@ -7,8 +7,11 @@ module.exports={
         // 1.原生的抛出错误
         // throw new Error('error')
         // 2.koa封装的抛出错误的函数
-        return ctx.throw({
-            code:40011,message:'error2'
+        // return ctx.throw({
+        //     code:40011,message:'error2'
+        // })
+        ctx.verifyParams({
+            username:'sss'
         })
         ctx.body={
             msg:'hello test'
